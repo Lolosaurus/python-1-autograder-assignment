2aa27d81054d4c58cdeb14805d780a7ec5606b19
Luke Assaad <luke.assaad@dawsoncollege.qc.ca>
2024-09-25 17:24:49 -0400

    added all variables

---
+++ autograder.py
@@ -0,0 +1,12 @@
+labs = int(input('Enter the number of labs completed: '))
+quiz = int(input('Enter the number of quizzes completed: '))
+assignment_1 = int(input('Enter grade for Assignment 1: '))
+assignment_2 = int(input('Enter grade for Assignment 2: '))
+assignment_3 = int(input('Enter grade for Assignment 3: '))
+assignment_4 = int(input('Enter grade for Assignment 4: '))
+assignments_avg = (assignment_1 + assignment_2 + assignment_3 + assignment_4)/4
+midterm_1 = int(input('Enter grade for Midterm 1: '))
+midterm_2 = int(input('Enter grade for Midterm 2: '))
+final = int(input('Enter grade for Final Exam: '))
+preps = int(input('Enter grade for Midterms and Final Preparation: '))
+grade = 
